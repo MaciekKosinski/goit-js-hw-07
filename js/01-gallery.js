3e93e5fc3e05707f36ce70661ddd5b49040e052d
Maciek Kosinski <maciek.kosinski87@gmail.com>
2022-09-28 19:16:32 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const photoGallery = document.querySelector(".gallery");
+photoGallery.innerHTML = makeGallery(galleryItems);
+
+function makeGallery(item) {
+    return item
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                    </div>`;
+        })
+        .join(" ")
+};
+
+photoGallery.addEventListener("click", openPhotoModal);
+
+let photoInstance;
+function openPhotoModal(event) {
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    event.preventDefault();
+
+    photoInstance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" 
+    width="800" 
+    height="600">`,
+        {
+            onShow: (photoInstance) => {
+                add();
+                function add() {
+                    window.addEventListener("keydown", escapeClick);
+                };
+            },
+            onClose: (photoInstance) => {
+                close();
+                function close() {
+                    window.removeEventListener("keydown", escapeClick);
+                };
+            },
+        },
+    );
+    photoInstance.show();
+};
+
+function escapeClick(event) {
+    if (event.code === "Escape") {
+        photoInstance.close()
+    };
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const photoGallery = document.querySelector(".gallery");
+photoGallery.innerHTML = makeGallery(galleryItems);
+
+function makeGallery(item) {
+    return item
+        .map(({ preview, original, description }) => {
+            return `<li class="gallery__list">
+                    <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" 
+                    alt="${description}" />
+                    </a>
+                    </li>`;
+        })
+        .join(" ")
+};
+
+var lightboxGalleryModal = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    animationSpeed: 500,
+    disableRightClick: true,
+});
